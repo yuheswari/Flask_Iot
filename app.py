bec80104c0d45f47776934fda6a6a58949427e0c
yuheswari v <yuheswari2525@gmail.com>
2025-01-25 04:48:12 +0000

    modified the dashboard

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import os
 import math
 from src import get_config
 basename = get_config("basename")
+app = Flask(__name__, static_folder='assets', static_url_path=basename)
 
-app = Flask(__name__,static_folder='assets',static_url_path= basename)
 
 @app.route(basename+"/dashboard")
 def dashboard():
    session ={
        "authenticated":True,
-       "username":"yuheswari2525",
+       "username":"yuheswari2525"
    }
-   return render_template('_master.html',session=session)
+   return render_template('dashboard.html',session=session)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
